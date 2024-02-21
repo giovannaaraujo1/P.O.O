f26dfa9b4d89184883373f61a5381d8c6c83403d
Giovanna Araujo <131299949+giovannaaraujo1@users.noreply.github.com>
2024-02-21 08:49:49 -0300

    Add files via upload

---
+++ Q5/Reserva.java
@@ -0,0 +1,13 @@
+public class Reserva {
+
+    String nome;
+    String tpquarto;
+    int noites;
+
+
+    public void reservas () {
+        System.out.println(nome  + " está hospedado(a) em quarto do tipo: "  + tpquarto +  " e utilizando-o por " 
+         +  noites  +  " noites."  );
+    }
+    
+}
