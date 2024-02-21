61c6aeec7fe18d9e85d20a5bcd2e3f650c20f9aa
Giovanna Araujo <131299949+giovannaaraujo1@users.noreply.github.com>
2024-02-21 09:23:49 -0300

    Add files via upload

---
+++ Q6/Veiculo.java
@@ -0,0 +1,12 @@
+public class Veiculo {
+
+    String placa;
+    String marca;
+    int ano;
+
+    public void infoveiculo () {
+        System.out.println(" O veículo de placa " + placa + " é da marca " + marca + 
+        " e do ano " + ano + ".");
+    }
+    
+}
