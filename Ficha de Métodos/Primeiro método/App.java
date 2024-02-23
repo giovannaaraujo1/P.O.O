968cc926aa2bcdf991d29866b2cc51104ed9e58e
Giovanna Araujo <131299949+giovannaaraujo1@users.noreply.github.com>
2024-02-23 08:50:59 -0300

    Add files via upload

---
+++ Ficha de Métodos/Primeiro método/App.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main (String[]args) {
+
+        // Disciplina d1 = new Disciplina ();
+
+        // d1.nome = "Programação Orientada a Objeto";
+        // d1.curso = "Sistemas para Internet";
+        // d1.periodo = "3°";
+        // d1.professor = "Iago Richard";
+        // d1.eletivaouN = false;
+
+        Pessoa p1 = new Pessoa();
+        p1.nome = "Giovanna";
+        p1.cpf = "111.222.333-00";
+        p1.idade = 21;
+        p1.altura = 1.61;
+        p1.matriculado = true;
+        p1.turno = "manhã";
+        p1.curso = "Sistemas para internet";
+        p1.professor = "Iago Richard";
+        p1.apresentacao();
+
+
+
+
+    }
+}
---
+++ Ficha de Métodos/Primeiro método/Pessoa.java
@@ -0,0 +1,26 @@
+public class Pessoa {
+
+    // Definindo atributo
+    String nome;
+    String cpf;
+    int idade;
+    boolean matriculado;
+    double altura;
+    String curso;
+    String turno;
+    String professor;
+
+    // Definindo métodos
+
+//     void comer (String comida){
+//         System.out.println( nome + " está comendo " + comida);
+//     }
+
+ void apresentacao () {
+    System.out.println("Meu nome é " + nome + ", tenho " + idade + " anos," + " estudo no curso de " + curso 
+    + " e meu turno é pela " + turno + "." );
+ }
+
+
+
+}
